65a1aa68f604f59335d5719df13be53ad6ba9138
lc <137232345@qq.com>
2020-12-03 08:22:18 -0800

    123
    Signed-off-by: lc <137232345@qq.com>

--- fsm_study/TutorialConfig.h
+++ fsm_study/TutorialConfig.h
@@ -1,8 +1,11 @@
 /*
  * @Author: your name
- * @Date: 2020-12-03 03:20:52
- * @LastEditTime: 2020-12-03 04:24:34
+ * @Date: 2020-12-03 03:22:27
+ * @LastEditTime: 2020-12-03 03:22:34
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: /study_project/fsm_study/TutorialConfig.h
+ * @FilePath: /study_project/fsm_study/TotorialConfig.h.in
  */
+// 与 tutorial 相关的配置好的选项与设置；
+#define Tutorial_VERSION_MAJOR @Tutorial_VERSION_MAJOR @
+#define Tutorial_VERSION_MINOR @Tutorial_VERSION_MINOR @
