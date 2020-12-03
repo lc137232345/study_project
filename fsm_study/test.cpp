2d09d8bf7dec67432230f357dc9338677f4bb838
lc <lc@localhost.localdomain>
2020-12-02 22:34:44 -0800

    add files

---
+++ fsm_study/test.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-02 22:29:18
+ * @LastEditTime: 2020-12-02 22:29:48
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /study_project/fsm_study/test.cpp
+ */
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+    std::cout << "/* message */" << std::endl;
+    return 0;
+}
