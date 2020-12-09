8cb2cf1b0118c983cc793d27f7eb791fb5d2bf84
lc <lc@localhost.localdomain>
2020-12-08 17:07:05 -0800

    123

--- fsm_study/src/main.cpp
+++ fsm_study/src/main.cpp
@@ -1,19 +1,26 @@
 /*
  * @Author: your name
  * @Date: 2020-12-05 06:34:10
- * @LastEditTime: 2020-12-06 05:43:57
+ * @LastEditTime: 2020-12-08 03:20:07
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /code/study_project/fsm_study/src/main.cpp
  */
 #include <iostream>
 #include "demo.h"
+void test(const int &a)
+{
+    std::cout << a << std::endl;
+    //a = 11;
+}
 int main(int argc, char const *argv[])
 {
     ue_conn_state *ue_conn_stat = new ue_conn_state;
 
     const double pi = 3.14;
-
+    const int a = 10;
+    test(a);
+    std::cout << a << std::endl;
     const double *cptr = &pi;
     double dval = 12.5;
     cptr = &dval;
