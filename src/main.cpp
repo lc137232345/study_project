cc205e14cefec1473075b0ce255f686a95bf8432
lc <137232345@qq.com>
2021-07-16 08:33:58 -0700

    更改了结构

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-14 18:30:48
- * @LastEditTime: 2021-07-15 19:03:49
+ * @LastEditTime: 2021-07-16 08:08:39
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /study_project/src/main.cpp
